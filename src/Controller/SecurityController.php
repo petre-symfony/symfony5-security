15eb6b5b3cb4f36f2a9e67b7d91acde1cd298ee8
petrero <petrero@gmail.com>
2021-11-29 17:33:51 +0200

    9.3. When Authentication Fails
      AuthenticationUtils: Rendering the Error
      - Cool! Next, inside login() controller, we need to read that error and render it. The most straightforward way to do that would be to grab the session and read out this key. But... it's even easier than that! Symfony provides a service that will grab the key from the session automatically. Add a new argument type-hinted with AuthenticationUtils
    
      And then give render() a second argument. Let's pass an error variable to Twig set to $authenticationUtils->getLastAuthenticationError()
    
      That's just a shortcut to read that key off of the session.
    
      This means that the error variable is literally going to be an AuthenticationException object. And remember, to figure out what went wrong, all AuthenticationException objects have a getMessageKey() method that returns an explanation.
    
      In templates/security/login.html.twig, let's render that. Right after the h1, say if error, then add a div with alert alert-danger. Inside render error.messageKey
    
      You don't want to use error.message because if you had some sort of internal error - like a database connection error - that message might contain sensitive details. But error.messageKey is guaranteed to be safe.
    
      Testing time! Refresh! Yes! We're redirected back to /login and we see:
    
        Username could not be found.
    
      That's the message if the User object can't be loaded: the error that comes form UserNotFoundException. It's... not a great message... since our users are logging in with an email, not a username.
    
      So next, let's learn how to customize these error messages and add a way to log out.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController {
 	/**
 	 * @Route("/login", name="app_login")
 	 */
-	public function login(): Response {
-		return $this->render('security/login.html.twig');
+	public function login(AuthenticationUtils $authentication): Response {
+		return $this->render('security/login.html.twig', [
+			'error' => $authentication->getLastAuthenticationError()
+		]);
 	}
 }
