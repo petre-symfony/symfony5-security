5bf2692c485983ecbb529f824b5fd82d7eb2a017
petrero <petrero@gmail.com>
2021-11-29 13:40:03 +0200

    7.1 Custom User Query & Credentials
      On the screen, we see a dd() of the password I entered into the login form and the User entity object for the email I entered. Something, somehow knew to take the submitted email and query for the User!
    
      UserBadge & The User Provider
      - Here's how this works. After we return the Passport object, the security system tries to find the User object from the UserBadge. If you just pass one argument to UserBadge - like we are - then it does this by leveraging our user provider. Remember that thing in security.yaml called providers?
    
      34 lines  config/packages/security.yaml
      security:
        providers:
            # used to reload user from session & other features (e.g. switch_user)
            app_user_provider:
                entity:
                    class: App\Entity\User
                    property: email
      Because our User class is an entity, we're using the entity provider that knows how to load users using the email property. So basically this is an object that's really good at querying the user table via the email property. So when we pass just the email to the UserBadge, the user provider uses that to query for the User.
    
      If a User object is found, Symfony then tries to "check the credentials" on our passport. Because we're using CustomCredentials, this means that it executes this callback... where we're dumping some data. If a User could not be found - because we entered an email that isn't in the database - authentication fails. More on both of these situations soon.
    
      Custom User Query
      - Anyways, the point is this: if you just pass one argument to UserBadge, the user provider loads the user automatically. That's the easiest thing to do. And you can even customize this query a bit if you need to - search for "Using a Custom Query to Load the User" on the Symfony docs to see how.
    
      Or... you can write your own custom logic to load the user right here. To do that, we're going to need the UserRepository. At the top of the class, add public function __construct()... and autowire a UserRepository argument. I'll hit Alt+Enter and select "Initialize properties" to create that property and set it
    
      Down in authenticate(), UserBadge has an optional second argument called a user loader. Pass it a callback with one argument: $userIdentifier
    
      It's pretty simple: if you pass a callable, then when Symfony loads your User, it will call this function instead of your user provider. Our job here is to load the user and return it. The $userIdentifier will be whatever we passed to the first argument of UserBadge... so the email in our case.
    
      Say $user = $this->userRepository->findOneBy() to query for email set to $userIdentifier
    
      This is where you could use whatever custom query you want. If we can't find the user, we need to throw a special exception. So if not $user, throw new UserNotFoundException(). That will cause authentication to fail. At the bottom, return $user
    
      This... is basically identical to what our user provider was doing a minute ago... so it won't change anything. But you can see how we have the power to load the User however we want to.
    
      Let's refresh. Yup! The same dump as before.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,10 +3,12 @@
 namespace App\Security;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -14,6 +16,13 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator {
+	private UserRepository $repository;
+
+	public function __construct(UserRepository $repository){
+
+		$this->repository = $repository;
+	}
+
 	public function supports(Request $request): ?bool {
 		return $request->getPathInfo() === '/login' && $request->isMethod('POST');
 	}
@@ -23,7 +32,15 @@ class LoginFormAuthenticator extends AbstractAuthenticator {
 		$password = $request->request->get('password');
 
 		return new Passport(
-			new UserBadge($email),
+			new UserBadge($email, function($userIdentifier) {
+				$user = $this->repository->findOneBy(['email' => $userIdentifier]);
+
+				if(!$user) {
+					throw new UserNotFoundException();
+				}
+
+				return $user;
+			}),
 			new CustomCredentials(function($credentials, User $user) {
 				dd($credentials, $user);
 			}, $password)
