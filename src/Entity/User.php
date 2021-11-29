78737923997a10c8bc5f923043b8c329a1a6093a
petrero <petrero@gmail.com>
2021-11-29 11:50:21 +0200

    3.1 Customizing the User Class
      What's cool about the User class is that... it's our class! As long as we implement UserInterface, we can add whatever else we want
      For example, I'd like to store the first name of my users. So let's go add a property for that. At your terminal, run:
    
        symfony console make:entity
      We'll edit the User entity, add a firstName property, have it be a string, 255 length... and say "yes" to nullable. Let's make this property optional in the database.
    
      Done! Back over in the User class, no surprises! We have a new property... and new getter and setter methods
    
      Go generate a migration for our new User. At the terminal, run
    
        symfony console make:migration
      Cool! Spin over and open that up to make sure it's not hiding any surprises
    
      Awesome: CREATE TABLE user with id, email, roles and first_name columns. Close this... and run it:
    
        symfony console doctrine:migrations:migrate
      Success!

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -28,6 +28,11 @@ class User implements UserInterface {
 	 */
 	private $roles = [];
 
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=true)
+	 */
+	private $firstName;
+
 	public function getId(): ?int {
 		return $this->id;
 	}
@@ -99,5 +104,15 @@ class User implements UserInterface {
 	public function eraseCredentials() {
 		// If you store any temporary, sensitive data on the user, clear it here
 		// $this->plainPassword = null;
+	}
+
+	public function getFirstName(): ?string {
+		return $this->firstName;
+	}
+
+	public function setFirstName(?string $firstName): self {
+		$this->firstName = $firstName;
+
+		return $this;
 	}
 }
