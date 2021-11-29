42d9a31ad91b8c3076964fec5f0c3840fd36cc33
petrero <petrero@gmail.com>
2021-11-29 12:04:07 +0200

    3.2 Customizing the User Class
      Adding User Fixtures
      - And because the User entity is... just a normal Doctrine entity, we can also add dummy users to our database using the fixtures system.
    
      Open up src/DataFixtures/AppFixtures.php. We're using Foundry to help us load data. So let's create a new Foundry factory for User. Since we're having SO much fun running commands in this video, let's sneak in one... or three more:
    
        symfony console make:factory
      Yup! We want one for User. Go open it up: src/Factory/UserFactory.php
    
      Our job in getDefaults() is to make sure that all of the required properties have good default values. Set email to self::faker()->email(), I won't set any roles right now and set firstName to self::faker()->firstName()
    
      Cool! Over in AppFixtures, at the bottom, create a user: UserFactory::createOne(). But use a specific email so we can log in using this later. How about, abraca_admin@example.com
    
      Then, to fill out the system a bit, add UserFactory::createMany(10)
    
      Let's try it! Back at the terminal, run:
    
        symfony console doctrine:fixtures:load
      No errors! Check out the new table:
    
        symfony console doctrine:query:sql 'SELECT * FROM user'
      And... there they are! Now that we have users in the database, we need to add one or more ways for them to authenticate. It's time to build a login form!

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,6 +9,7 @@ use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use App\Factory\QuestionTagFactory;
 use App\Factory\TagFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -40,6 +41,9 @@ class AppFixtures extends Fixture {
         'question' => $questions[array_rand($questions)]
       ];
     })->needsApproval()->many(20)->create();
+
+    UserFactory::createOne(['email' => 'abraca_admin@examole.com']);
+    UserFactory::createMany(10);
 
     $manager->flush();
   }
