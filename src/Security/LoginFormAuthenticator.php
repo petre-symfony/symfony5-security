e6b2ec44aff804c8ea2680a4bf996802400de7ee
petrero <petrero@gmail.com>
2021-11-30 11:20:28 +0200

    15.2. Always Remember Me & "signature_properties"
      Enabling on the RememberMeBadge
      - The other way that you can enable the remember me cookie in all situations is via the badge itself. Comment-out the new option. Well... let me fix my typo and then comment it out
    
      Inside of LoginFormAuthenticator, on the badge itself, you can call ->enable()... which returns the badge instance
    
      This says:
    
        I don't care about any other settings or the checkbox: I definitely want the remember me system to add a cookie.
    
      Let's try it! Clear the session and REMEMBERME cookie. This time when we login... oh, invalid CSRF token! That's because I just killed my session without refreshing - silly Ryan! Refresh and try again.
    
      Beautiful! We have the REMEMBERME cookie!

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -55,7 +55,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator {
 					'authenticate',
 					$request->request->get('_csrf_token')
 				),
-				new RememberMeBadge()
+				(new RememberMeBadge())->enable()
 			]
 		);
 	}
