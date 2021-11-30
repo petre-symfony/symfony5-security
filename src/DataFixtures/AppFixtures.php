4653cc78b972547914231d9a7ce089e161777afe
petrero <petrero@gmail.com>
2021-11-30 18:12:18 +0200

    22.1. Dynamic Roles
      Earlier, we talked about how the moment a user logs in, Symfony calls the getRoles() method on the User object to figure out which roles that user will have
    
      This method reads a $roles array property that's stored in the database as JSON... then always adds ROLE_USER to it.
    
      Until now, we haven't given any users any extra roles in the database... so all users have just ROLE_USER. You can see this in the web debug toolbar: click to jump into the profiler. Yup, we have ROLE_USER.
    
      This is too boring... so let's add some true admin users! First, open config/packages/security.yaml... and, down under access_control, change this to once again require ROLE_ADMIN
    
      Remember: roles are just strings that we invent... they can be anything: ROLE_USER ROLE_ADMIN, ROLE_PUPPY, ROLE_ROLLERCOASTER... whatever. The only rule is that they must start with ROLE_. Thanks to this, if we go to /admin... access denied!
    
      Populating Roles in the Database
      - Let's add some admin users to the database. Open up the fixtures class: src/DataFixtures/AppFixtures.php. Let's see... down here, we're creating one custom user and then 10 random users. Make this first user an admin: set roles to an array with ROLE_ADMIN
    
      Let's also create one normal user that we can use to log in. Copy the UserFactory code, paste, use abraca_user@example.com... and leave roles empty
    
      Let's do it! At your terminal, run:
    
        symfony console doctrine:fixtures:load
      When that finishes... spin over and refresh. We got logged out! That's because, when the user was loaded from the session, our user provider tried to refresh the user from the database... but the old user with its old id was gone thanks to the fixtures. Log back in.... with password tada and... access granted! We rock! And in the profiler, we have the two roles.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,38 +13,48 @@ use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture {
-  public function load(ObjectManager $manager) {
-    TagFactory::createMany(100);
+class AppFixtures extends Fixture
+{
+	public function load(ObjectManager $manager)
+	{
+		TagFactory::createMany(100);
 
-    $questions = QuestionFactory::createMany(20);
+		$questions = QuestionFactory::createMany(20);
 
-    QuestionTagFactory::createMany(100, function () {
-      return [
-        'tag' => TagFactory::random(),
-        'question' => QuestionFactory::random(),
-      ];
-    });
+		QuestionTagFactory::createMany(100, function () {
+			return [
+				'tag' => TagFactory::random(),
+				'question' => QuestionFactory::random(),
+			];
+		});
 
-    QuestionFactory::new()
-      ->unpublished()
-      ->many(5)
-      ->create();
+		QuestionFactory::new()
+			->unpublished()
+			->many(5)
+			->create();
 
-    AnswerFactory::createMany(100, function () use ($questions) {
-      return [
-        'question' => $questions[array_rand($questions)]
-      ];
-    });
-    AnswerFactory::new(function () use ($questions) {
-      return [
-        'question' => $questions[array_rand($questions)]
-      ];
-    })->needsApproval()->many(20)->create();
+		AnswerFactory::createMany(100, function () use ($questions) {
+			return [
+				'question' => $questions[array_rand($questions)]
+			];
+		});
+		AnswerFactory::new(function () use ($questions) {
+			return [
+				'question' => $questions[array_rand($questions)]
+			];
+		})->needsApproval()->many(20)->create();
 
-    UserFactory::createOne(['email' => 'abraca_admin@example.com']);
-    UserFactory::createMany(10);
+		UserFactory::createOne([
+			'email' => 'abraca_admin@example.com',
+			'roles' => ['ROLE_ADMIN']
+		]);
 
-    $manager->flush();
-  }
+		UserFactory::createOne([
+			'email' => 'abraca_user@example.com'
+		]);
+
+		UserFactory::createMany(10);
+
+		$manager->flush();
+	}
 }
