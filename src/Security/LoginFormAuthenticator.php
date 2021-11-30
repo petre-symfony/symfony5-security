7d46038d2b878f9e2ea0131f0d0b841f5f072ab9
petrero <petrero@gmail.com>
2021-11-30 12:43:04 +0200

    18.1. AbstractLoginFormAuthenticator & Redirecting to Previous URL
      I have a confession to make: in our authenticator, we did too much work! Yep, when you build a custom authenticator for a "login form", Symfony provides a base class that can make life much easier. Instead of extending AbstractAuthenticator extend AbstractLoginFormAuthenticator
    
      Hold Command or Ctrl to open that class. Yup, it extends AbstractAuthenticator and also implements AuthenticationEntryPointInterface. Cool! That means that we can remove our redundant AuthenticationEntryPointInterface
    
      The abstract class requires us to add one new method called getLoginUrl(). Head to the bottom of this class and go to "Code"->"Generate" - or Command+N on a Mac - and then "Implement Methods" to generate getLoginUrl(). For the inside, steal the code from above... and return $this->router->generate('app_login')
    
      The usefulness of this base class is pretty easy to see: it implements three of the methods for us! For example, it implements supports() by checking to see if the method is POST and if the getLoginUrl() string matches the current URL. In other words, it does exactly what our supports() method does. It also handles onAuthenticationFailure() - storing the error in the session and redirecting back to the login page - and also the entry point - start() - by, yet again, redirecting to /login.
    
      This means that... oh yea... we can remove code! Let's see: delete supports(), onAuthenticationFailure() and also start()
    
      Much nicer
    
      Let's make sure we didn't break anything: go to /admin and... perfect! The start() method still redirects us to /login. Let's log in with abraca_admin@example.com, password tada and... yes! That still works too: it redirects us to the homepage... because that's what we're doing inside of onAuthenticationSuccess

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -22,7 +22,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class LoginFormAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface {
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator {
 	private UserRepository $repository;
 	private RouterInterface $router;
 
@@ -30,10 +30,6 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
 
 		$this->repository = $repository;
 		$this->router = $router;
-	}
-
-	public function supports(Request $request): ?bool {
-		return $request->getPathInfo() === '/login' && $request->isMethod('POST');
 	}
 
 	public function authenticate(Request $request): PassportInterface {
@@ -67,18 +63,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator implements Authentica
 		);
 	}
 
-	public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response {
-		$request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-
-		return new RedirectResponse(
-			$this->router->generate('app_login')
-		);
-	}
-
-	public function start(Request $request, AuthenticationException $authException = null) {
-		return new RedirectResponse(
-			$this->router->generate('app_login')
-		);
+	protected function getLoginUrl(Request $request): string {
+		return $this->router->generate('app_login');
 	}
 
 }
