a69e3dee8fdd6cf032f6fd3c69a1a57c386afa2b
petrero <petrero@gmail.com>
2021-12-12 18:05:55 +0200

    31.1. Manual Authentication
      Our registration form would work if we tried it. But, after registration, I want to also automatically authenticate the user... so they don't need to register and then immediately log in... that would be silly.
    
      So far, all authentication has been done... kind of indirectly: the user makes a request, some authenticator handles it and... voilà! But in this case, we want to authenticate the user directly, by writing code inside of a controller.
    
      Hello UserAuthenticatorInterface
      - And... this is totally possible, by autowiring a service specifically for this. Add a new argument up here type-hinted with UserAuthenticatorInterface and I'll call it $userAuthenticator
    
      This object allows you to just... authenticate any User object. Right before the redirect, let's do that: $userAuthenticator->authenticateUser() and we need to pass this a few arguments. The first one is the User we want to authenticate
    
      Easy. The second is an "authenticator" that you want to use. This system works by basically taking your User object and... kind of "running it through" one of your authenticators.
    
      If we were still using our custom LoginFormAuthenticator, passing this argument would be really easy. We could just autowire the LoginFormAuthenticator service up here and pass it in.
    
      Injecting the Service for form_login
      - But, in our security.yaml file, our main way of authenticating is form_login:
    
     61 lines  config/packages/security.yaml
     security:
        firewalls:
            main:
                form_login:
                    login_path: app_login
                    check_path: app_login
                    username_parameter: email
                    password_parameter: password
                    enable_csrf: true
      That does activate an authenticator service behind the scenes - just like our custom LoginFormAuthenticator. The tricky part is figuring out what that service is and injecting it into our controller.
    
      So, we need to do a bit of digging. At your terminal, run
    
        symfony console debug:container
      and search for form_login:
    
        symfony console debug:container form_login
      In this list, I see a service called security.authenticator.form_login.main... and remember that "main" is the name of our firewall. This is the id of the service that we want. If you're wondering about the service above this, if you checked, you'd find that it's an "abstract" service. A, sort of "fake" service that's used as a template to create the real service for any firewalls that use form_login.
    
      Anyways, I'll hit "1" to get more details. Ok cool: this service is an instance of FormLoginAuthenticator, which is the core class that we looked at earlier.
    
      Back in our controller, add another argument type-hinted with FormLoginAuthenticator
    
      Then, down here, pass the new argument to authenticateUser()
    
      This won't work yet, but stick with me.
    
      The final argument to authenticateUser() is the Request object... which we already have... it's our first controller argument
    
      authenticateUser Returns a Response
      - Done! Oh, and one cool thing about authenticateUser() is that it returns a Response object! Specifically, the Response object from the onAuthenticationSuccess() method of whatever authenticator we passed in. This means that instead of redirecting to the homepage, we can return this and, wherever that authenticator normally redirects to, we will redirect there as well, which could be the "target path".

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,12 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 
 class RegistrationController extends AbstractController {
 	/**
 	 * @Route("/register", name="app_register")
 	 */
-	public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface): Response {
+	public function register(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, UserAuthenticatorInterface $userAuthenticator, FormLoginAuthenticator $formLoginAuthenticator): Response {
 		$user = new User();
 		$form = $this->createForm(RegistrationFormType::class, $user);
 		$form->handleRequest($request);
@@ -33,7 +35,11 @@ class RegistrationController extends AbstractController {
 			$entityManager->flush();
 			// do anything else you need here, like send an email
 
-			return $this->redirectToRoute('app_homepage');
+			$userAuthenticator->authenticateUser(
+				$user,
+				$formLoginAuthenticator,
+				$request
+			);
 		}
 
 		return $this->render('registration/register.html.twig', [
