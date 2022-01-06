3b139273ded8a57d81ea1e531214067a359f1737
petrero <petrero@gmail.com>
2022-01-06 20:06:15 +0200

    33.3.Leveraging the Question Owner
      So inside of QuestionController, we need a security check. We first need to make sure that the user is logged in. Do that with $this->denyAccessUnlessGranted() passing IS_AUTHENTICATED_REMEMBERED
    
      Thanks to this, we're guaranteed to get a User object if we say $this->getUser(). We can use that: if $question->getOwner() does not equal $this->getUser(), then someone other than the owner is trying to access this page. Deny access with throw $this->createAccessDeniedException(). I'll say:
    
        You are not the owner!
    
      But, remember, these error messages are only shown to developers
    
      Ok, so right now I'm not logged in at all. So if we refresh, it kicks us back to the login page. So... yay! We just successfully prevented anyone other than the owner from accessing this edit page!
    
      But... bad news friends: I don't like this solution. I don't like putting any manual security logic inside my controller. Why? Because it means that we're going to need to repeat that logic in Twig in order to hide or show the edit button. And what if our logic gets more complex? What if you can edit a question if you're the owner or if you have ROLE_ADMIN? Now we would need to update and maintain the duplicate logic in two places at least. Nope, we do not want to duplicate our security rules.
    
      So next let's learn about the voter system, which is the key to centralizing all of this authorization logic in a beautiful way.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -68,6 +68,11 @@ class QuestionController extends AbstractController {
 	 * @Route("/questions/edit/{slug}", name="app_question_edit")
 	 */
 	public function edit(Question $question) {
+		$this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+		if ($question->getOwner() !== $this->getUser()) {
+			throw $this->createAccessDeniedException('You are not the owner!');
+		}
 		return $this->render('question/edit.html.twig', [
 			'question' => $question,
 		]);
