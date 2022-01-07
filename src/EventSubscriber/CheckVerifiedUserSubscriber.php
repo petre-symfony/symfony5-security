0293222e9999087a6f52a9a18b208fcf6e27e802
petrero <petrero@gmail.com>
2022-01-07 19:36:51 +0200

    40.2. Creating a Security Event Subscriber
      The point is: we want our code to run after CheckCredentialsListener. To do that, we can give our listener a negative priority. Tweak the syntax: set the event name to an array with the method name as the first key and the priority as the second. How about negative 10.
    
    Thanks to this, the user will need to enter a valid email and a valid password before our listener is called. Try it: go back to abraca_admin@example.com, password tada and... beautiful!
    
      Using the Event Object
      - Check out the event object that we're passed: it's full of good stuff. It contains the authenticator that was used, in case we need to do something different based on that. It also holds the Passport... which is huge because that contains the User object and badges... because sometimes you need to do different things based on the badges on the passport.
    
      Inside of our subscriber, let's get to work. To get the user, we first need to get the passport: $passport = $event->getPassport(). Now, add if not  is an instanceof UserPassportInterface, throw an exception.
    
      This check isn't important and is not needed in Symfony 6 and higher. Basically, this check makes sure that our Passport has a getUser() method, which in practice, it always will. In Symfony 6, the check isn't needed at all because the Passport class literally always has this method.
    
      This means that, down here, we can say $user = $passport->getUser(). And then let's add a sanity check: if $user is not an instance of our User class, throw an exception: "Unexpected user type".
    
      In practice, in our app, this isn't possible. But that's a nice way to hint to my editor - or static analysis tools - that  is our User class. Thanks to this, when we say if not $user->getIsVerified(), it auto-completes that method.
    
      Failing Authentication
      - Ok, if we are not verified, we need to cause authentication to fail. How do we do that? It turns out that, at any time during the authentication process, we can throw an AuthenticationException - from Security - and that will cause authentication to fail.
    
      And there are a bunch of subclasses to this class, like BadCredentialsException. You can throw any of these because they all extend AuthenticationException.
    
      Check it out. Let's refresh and... got it!
    
        An authentication exception occurred.
    
      That's the generic error message tied to the AuthenticationException class... not a very good error message. But it did get the job done.

--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -2,17 +2,34 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface {
 	public function onCheckPassport(CheckPassportEvent $event) {
-		dd($event);
+		$passport = $event->getPassport();
+
+		if(!$passport instanceof UserPassportInterface) {
+			throw new \Exception('Unexpected passport type');
+		}
+
+		$user = $passport->getUser();
+
+		if (!$user instanceof User) {
+			throw new \Exception('Unexpected user type');
+		}
+
+		if(!$user->getIsVerified()){
+			throw new AuthenticationException();
+		}
 	}
 
 	public static function getSubscribedEvents() {
 		return [
-			CheckPassportEvent::class => 'onCheckPassport'
+			CheckPassportEvent::class => ['onCheckPassport', -10 ]
 		];
 	}
 
