6b09edb1de61d95b69514c4f92d5ea4a67b364aa
petrero <petrero@gmail.com>
2022-01-07 20:17:01 +0200

    41.3. Custom Redirect when "Email Not Verified"
      Redirecting when Account is Not Verified
      - So, if not $event->getException() is an instance of AccountNotVerifiedAuthenticationException, then just return and allow the default failure behavior to do its thing.
    
      Finally, down here, we know that we should redirect to that custom page. Let's... go create that page real quick. Do it in src/Controller/RegistrationController.php. Down at the bottom, add a new method. I'll call it resendVerifyEmail(). Above this, add @Route() with, how about /verify/resend and name equals app_verify_resend_email. Inside, I'm just going to render a template: return $this->render(), registration/resend_verify_email.html.twig.
    
      Let's go make that! Inside of templates/registration/, create resend_verify_email.html.twig. I'll paste in the template: there's nothing fancy here at all. It just explains the situation.
    
      I did include a button to resend the email, but I'll leave the implementation to you. I'd probably surround it with a form that POSTs to this URL. And then, in the controller, if the method is POST, I’d use the verify email bundle to generate a new link and re-send it. Basically the same code we used after registration.
    
      Anyways, now that we have a functional page, copy the route name and head back to our subscriber. To override the normal failure behavior, we can use a setResponse() method on the event.
    
      Start with $response = new RedirectResponse() - we're going to generate a URL to the route in a minute - then $event->setResponse($response).
    
      To generate the URL, we need a __construct() method - let me spell that correctly - with a RouterInterface $router argument. Hit Alt+Enter and go to initialize properties to create that property and set it.
    
      Back down here, we're in business: $this->router->generate() with app_verify_resend_email.
    
      Donezo! We fail authentication, our first listener throws the custom exception, we look for that exception from the LoginFailureEvent listener… and set the redirect.
    
      Testing time! Refresh and... got it! We're sent over to /verify/resend. I love that!
    
      Next: let's finish this tutorial by doing something super cool, super fun, and... kinda nerdy. Let's add two-factor authentication, complete with fancy QR codes.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -85,4 +85,11 @@ class RegistrationController extends AbstractController {
 
 		return $this->redirectToRoute('app_login');
 	}
+
+	/**
+	 * @Route("/verify/resend", name="app_verify_resend_email")
+	 */
+	public function resendVerifyEmail(){
+		return $this->render('registration/resend_verify_email.html.twig');
+	}
 }
--- src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -5,11 +5,20 @@ namespace App\EventSubscriber;
 use App\Entity\User;
 use App\Security\AccountNotVerifiedAuthenticationException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Http\Event\CheckPassportEvent;
 use Symfony\Component\Security\Http\Event\LoginFailureEvent;
 
 class CheckVerifiedUserSubscriber implements EventSubscriberInterface {
+	private RouterInterface $router;
+
+	public function __construct(RouterInterface $router) {
+
+		$this->router = $router;
+	}
+
 	public function onCheckPassport(CheckPassportEvent $event) {
 		$passport = $event->getPassport();
 
@@ -31,7 +40,13 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface {
 	}
 
 	public function onFailureEvent(LoginFailureEvent $event){
-		dd($event);
+		if (!$event->getException() instanceof AccountNotVerifiedAuthenticationException) {
+			return;
+		}
+
+		$response = new RedirectResponse($this->router->generate('app_verify_resend_email'));
+
+		$event->setResponse($response);
 	}
 
 	public static function getSubscribedEvents() {
